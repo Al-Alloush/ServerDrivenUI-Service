252c73d427562109f289565ea157b840b0da5f16
ahmadalalloush <al-alloush@outlook.com>
2025-12-14 12:20:08 +0100

    feat: add getters and setters for ThemePalette and ColorThemeWrapperDto classes

--- src/main/java/dev/codexo/app/srv/serverdrivenui/imeterrecorder/model/ThemePalette.java
+++ src/main/java/dev/codexo/app/srv/serverdrivenui/imeterrecorder/model/ThemePalette.java
@@ -24,4 +24,68 @@ public class ThemePalette {
         this.secondaryDarkText = secondaryDarkText;
         this.tertiary = tertiary;
     }
+
+    public String getPrimary() {
+        return primary;
+    }
+
+    public void setPrimary(String primary) {
+        this.primary = primary;
+    }
+
+    public String getPrimaryColor() {
+        return primaryColor;
+    }
+
+    public void setPrimaryColor(String primaryColor) {
+        this.primaryColor = primaryColor;
+    }
+
+    public String getPrimaryDark() {
+        return primaryDark;
+    }
+
+    public void setPrimaryDark(String primaryDark) {
+        this.primaryDark = primaryDark;
+    }
+
+    public String getPrimaryDarkText() {
+        return primaryDarkText;
+    }
+
+    public void setPrimaryDarkText(String primaryDarkText) {
+        this.primaryDarkText = primaryDarkText;
+    }
+
+    public String getSecondary() {
+        return secondary;
+    }
+
+    public void setSecondary(String secondary) {
+        this.secondary = secondary;
+    }
+
+    public String getSecondaryDark() {
+        return secondaryDark;
+    }
+
+    public void setSecondaryDark(String secondaryDark) {
+        this.secondaryDark = secondaryDark;
+    }
+
+    public String getSecondaryDarkText() {
+        return secondaryDarkText;
+    }
+
+    public void setSecondaryDarkText(String secondaryDarkText) {
+        this.secondaryDarkText = secondaryDarkText;
+    }
+
+    public String getTertiary() {
+        return tertiary;
+    }
+
+    public void setTertiary(String tertiary) {
+        this.tertiary = tertiary;
+    }
 }
